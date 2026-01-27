10a693ce468d11ea0c86ce5672d3d9d605e11439
emalang <ema.lang68@gmail.com>
2026-01-27 23:56:00 +0100

    Add Chrome WebDriver setup

--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -1,4 +1,14 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class DriverFactory {
+
+    public static WebDriver createDriver() {
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--start-maximized");
+        return new ChromeDriver(options);
+    }
 }
